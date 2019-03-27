cdeacaa5d2dc7cca409c795e469eb825c74ac444
Desiree Gunnoe <desireegunnoe@Desirees-MacBook-4.local>
2019-03-26 20:23:01 -0700

    Adding FourWheelDriveTask example

--- 2019/test/DesConstants.java
+++ 2019/test/DesConstants.java
@@ -2,4 +2,11 @@ package test;
 
 public class DesConstants {
     private static final int TICKS_PER_INCH = 79;
+    private static double SERVO_DOMAIN = 256.0;
+    public static double RIGHT_GRAB_OPEN =10/SERVO_DOMAIN;
+    public static double RIGHT_GRAB_CLOSED = 15/SERVO_DOMAIN;
+    public static double LEFT_GRAB_OPEN = 12/SERVO_DOMAIN;
+    public static double LEFT_GRAB_CLOSED = 17/SERVO_DOMAIN;
+
+
 }
